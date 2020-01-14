d89a20540bc2012fe79a86777e6920c855abf7d1
Thanakorn Kriangudom <45796358+bluebox-dev@users.noreply.github.com>
2020-01-14 14:26:51 +0700

    Update csv2ymal.py

--- csv2ymal.py
+++ csv2ymal.py
@@ -3,9 +3,15 @@ import yaml
 
 dict_file=[]
 df = pd.read_excel("Book1.xlsx")
-for i in range(df.shape[0]):
-    dict_file.append((df.loc[i].values).tolist())
+
+for i in range(df.shape[1]):
+    name = df.columns.values[i]
+    dict_file.append((df[name]).tolist())
+
+# print(dict_file)
 x = df.columns.values
 x = dict(zip(x, dict_file))
+
+# print(x)
 with open(r'ip.yaml', 'w') as file:
     documents = yaml.dump(x, file)
